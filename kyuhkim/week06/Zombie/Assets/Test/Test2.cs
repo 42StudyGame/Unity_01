607c2a9e99bbab069bbfd194f8baf801e653416d
desktop <kyuhkim@student.42seoul.kr>
2022-07-30 02:11:46 +0900

    add comment: scriptable object

--- kyuhkim/week06/Zombie/Assets/Test/Test2.cs
+++ kyuhkim/week06/Zombie/Assets/Test/Test2.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Test2 : MonoBehaviour
 {
     public Test test;
-
     public GameObject prefab;
+    public TextMeshProUGUI text;
     
     // Start is called before the first frame update
     private void Start()
     {
+        // test.life = 3;
         for (var i = 0; i < 100; ++i)
         {
-            Instantiate(prefab);
+            Instantiate(prefab, transform);
+            text.text = transform.childCount.ToString();
         }
 
         StartCoroutine(Kill());
@@ -27,5 +30,9 @@ public class Test2 : MonoBehaviour
             --test.life;
             Debug.LogWarning($"life = {test.life}");
         }
+
+        yield return new WaitForSecondsRealtime(1);
+        
+        text.text = transform.childCount.ToString();
     }
 }
