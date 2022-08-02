a82693e631530a7f31d73a235ecfa8e5b387fc36
LeeDanny06 <leedenny06@gmail.com>
2022-08-02 22:57:44 +0900

    HelloCode6.cs - for

---
+++ yeonjale/week02/HelloCode6.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloCode6 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Debug.Log(i + "번째 입니다!");
+        }
+    }
+}
