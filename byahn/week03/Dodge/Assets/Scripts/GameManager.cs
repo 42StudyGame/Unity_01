4eedb5cfe7365227c99558ea19fc073bc72f7039
nonoaa <nonoaa@kyonggi.ac.kr>
2022-08-10 19:05:30 +0900

    week03

---
+++ byahn/week03/Dodge/Assets/Scripts/GameManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject gameoverText;
+    public Text timeText;
+    public Text recordText;
+
+    private float surviveTime;
+    private bool isGameover;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        surviveTime = 0;
+        isGameover = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isGameover)
+        {
+            surviveTime += Time.deltaTime;
+            timeText.text = "Time : " + (int)surviveTime;
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(("SampleScene"));
+            }
+        }
+    }
+
+    public void EndGame()
+    {
+        isGameover = true;
+        gameoverText.SetActive(true);
+
+        float bestTime = PlayerPrefs.GetFloat("BestTime");
+
+        if (surviveTime > bestTime)
+        {
+            bestTime = surviveTime;
+            PlayerPrefs.SetFloat("BestTime", bestTime);
+        }
+
+        recordText.text = "Best Time: " + (int)bestTime;
+    }
+}
