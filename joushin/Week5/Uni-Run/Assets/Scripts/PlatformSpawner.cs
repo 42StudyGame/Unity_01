69fb41e872bf40851f677a7ba2b63fb6501b5ad7
haward <wndddms12345@naver.com>
2022-08-24 15:11:47 +0900

    5주차 joushin입니다!

---
+++ joushin/Week5/Uni-Run/Assets/Scripts/PlatformSpawner.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+// 발판을 생성하고 주기적으로 재배치하는 스크립트
+public class PlatformSpawner : MonoBehaviour {
+    public GameObject platformPrefab; // 생성할 발판의 원본 프리팹
+    public int count = 3; // 생성할 발판의 개수 이건 나중에 발판을 더 다양하게 하고 싶다면 변경해뵤기
+
+    public float timeBetSpawnMin = 1.25f; // 다음 배치까지의 시간 간격 최솟값
+    public float timeBetSpawnMax = 2.25f; // 다음 배치까지의 시간 간격 최댓값
+    private float timeBetSpawn; // 다음 배치까지의 시간 간격 
+
+    public float yMin = -3.5f; // 배치할 위치의 최소 y값
+    public float yMax = 1.5f; // 배치할 위치의 최대 y값
+    private float xPos = 20f; // 배치할 위치의 x 값
+
+    private GameObject[] platforms; // 미리 생성한 발판들
+    private int currentIndex = 0; // 사용할 현재 순번의 발판
+
+    private Vector2 poolPosition = new Vector2(0, -20); // 초반에 생성된 발판들을 화면 밖에 숨겨둘 위치
+    private float lastSpawnTime; // 마지막 배치 시점
+
+
+    void Start() {
+        // 변수들을 초기화하고 사용할 발판들을 미리 생성
+        // count만큼의 공간을 가지는 새로운 발판 배열 생성
+        platforms = new GameObject[count];
+        //count 만큼 루프하면서 발판 생성
+        for (int i = 0; i < count; i++)
+        {
+            //platformPrefeb을 원본으로 새 발판을 poolPosition 위치에 복제생성
+            //생성된 발판을 platform 배열에 할당
+            platforms[i] = Instantiate(platformPrefab, poolPosition, Quaternion.identity);
+        }
+        //마지막 배치시점 초기화
+        lastSpawnTime = 0f;
+        //다음번 배치까지의 시간 간격을 0으로 초기화
+        timeBetSpawn = 0f;
+    }
+
+    void Update() {
+        // 순서를 돌아가며 주기적으로 발판을 배치
+        if (GameManager.instance.isGameover)
+            return;
+        if (Time.time >= lastSpawnTime + timeBetSpawn)
+        {
+            //기록된 마지막 배치 시점을 현재 시점으로 갱신
+            lastSpawnTime = Time.time;
+            //다음 배치까지 시간간격을 랜덤하게 설정, 배치 위치 높이를 랜덤설정
+            timeBetSpawn = Random.Range(timeBetSpawnMin, timeBetSpawnMax);
+            float yPos = Random.Range(yMin, yMax);
+            //사용할 현재 순번의 발판 게임 오브젝트를 비활성화 하고 즉시 다시 활성화
+            //이떄 발판의 platform 컴포넌트의 OnEnalvble메서드가 실행됨.
+            platforms[currentIndex].SetActive(false);
+            platforms[currentIndex].SetActive(true);
+            
+            //현재 순번의 발판을 화면 오른쪽에 재배치
+            platforms[currentIndex].transform.position = new Vector2(xPos, yPos);
+            //순번 넘기기
+            currentIndex++;
+            
+            //마지막 순번에 도달했다면 순번을 리셋
+            if (currentIndex >= count)
+            {
+                currentIndex = 0;
+            }
+        }
+    }
+}
---
+++ joushin/Week5/Uni-Run/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+// PlayerController는 플레이어 캐릭터로서 Player 게임 오브젝트를 제어한다.
+public class PlayerController : MonoBehaviour {
+   public AudioClip deathClip; // 사망시 재생할 오디오 클립
+   public float jumpForce = 700f; // 점프 힘
+
+   private int jumpCount = 0; // 누적 점프 횟수
+   private bool isGrounded = false; // 바닥에 닿았는지 나타냄
+   private bool isDead = false; // 사망 상태
+
+   private Rigidbody2D playerRigidbody; // 사용할 리지드바디 컴포넌트
+   private Animator animator; // 사용할 애니메이터 컴포넌트
+   private AudioSource playerAudio; // 사용할 오디오 소스 컴포넌트
+   
+   private void Start() {
+       // 게임 오브젝트로부터 사용할 컴포넌트들을 가져와 변수에 할당
+       playerRigidbody = GetComponent<Rigidbody2D>();//만약 두개 있는경우? 어떻게 될까?
+       animator = GetComponent<Animator>();
+       playerAudio = GetComponent<AudioSource>();
+   }
+
+   private void Update() {
+       // 사용자 입력을 감지하고 점프하는 처리
+       if (isDead)
+           return;
+       //마우스 왼쪽 버튼이 눌렀으며 점프횟수가 2에 도달하지 않았다면
+       if (Input.GetMouseButtonDown(0) && jumpCount < 2)
+       {
+           //점프 횟수 증가
+           jumpCount++;
+           //점프 직전에 속도를 순간적으로 (0, 0)으로 변경
+           playerRigidbody.velocity = Vector2.zero;
+           //리자드 바디 위쪽으로 힘주기
+           playerRigidbody.AddForce(new Vector2(0, jumpForce));
+           //오디오 소스 재생
+           playerAudio.Play();
+       }//마우스 왼쪽에서 손을 떼는순간 && 속도의 y값이 양수라면 (위로 상승중)
+       else if (Input.GetMouseButtonUp(0) && playerRigidbody.velocity.y > 0)
+           //현재 속도를 절반으로 변경
+           playerRigidbody.velocity = playerRigidbody.velocity * 0.5f;
+       //애니매이터의 Grounded 파라미터를 isGrounded값으로 갱신.
+       animator.SetBool("Grounded",isGrounded);
+       
+   }
+   // 사망 처리
+   private void Die() {
+       //애니메이터의 Die 트리거 파라미터 셋
+       animator.SetTrigger("Die");
+       //오디오 소스에 할당된 오디오 클립을 deathClip으로 변경
+       playerAudio.clip = deathClip;
+       //사망 효과음 재생
+       playerAudio.Play();
+       
+       //속도를 제로(0,0)으로 변경
+       playerRigidbody.velocity = Vector2.zero;
+       //사망 상태를 true로 변경
+       isDead = true;
+       //게임 매니저의 게임오버 처리 실행
+       GameManager.instance.OnPlayerDead();
+   }
+   // 트리거 콜라이더를 가진 장애물과의 충돌을 감지
+   private void OnTriggerEnter2D(Collider2D other) {
+       if (other.tag == "Dead" && !isDead)
+           //충돌한 상대의 태그가 Dead이며 아직 사망하지 않았다면 Die실행
+           Die();
+   }
+   
+// 바닥에 닿았음을 감지하는 처리
+   private void OnCollisionEnter2D(Collision2D collision)
+   {
+       //어떤 콜라이더와 닿았으며, 충돌 표면이 위쪽을 보고 있으면
+       if (collision.contacts[0].normal.y > 0.7f)
+       {
+           isGrounded = true;
+           jumpCount = 0;
+       }
+   }
+   // 바닥에서 벗어났음을 감지하는 처리
+   private void OnCollisionExit2D(Collision2D collision)
+   {
+       isGrounded = false;
+   }
+}
