019f4bc0206e43e849f1e209fdd5afd3715c7478
nonoaa <nonoaa@kyonggi.ac.kr>
2022-09-07 01:31:19 +0900

    week07

---
+++ byahn/week07/Zombie/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using UnityEngine.UI; // UI 관련 코드
+
+// 플레이어 캐릭터의 생명체로서의 동작을 담당
+public class PlayerHealth : LivingEntity {
+    public Slider healthSlider; // 체력을 표시할 UI 슬라이더
+
+    public AudioClip deathClip; // 사망 소리
+    public AudioClip hitClip; // 피격 소리
+    public AudioClip itemPickupClip; // 아이템 습득 소리
+
+    private AudioSource playerAudioPlayer; // 플레이어 소리 재생기
+    private Animator playerAnimator; // 플레이어의 애니메이터
+
+    private PlayerMovement playerMovement; // 플레이어 움직임 컴포넌트
+    private PlayerShooter playerShooter; // 플레이어 슈터 컴포넌트
+
+    private void Awake() {
+        // 사용할 컴포넌트를 가져오기
+        playerAnimator = GetComponent<Animator>();
+        playerAudioPlayer = GetComponent<AudioSource>();
+
+        playerMovement = GetComponent<PlayerMovement>();
+        playerShooter = GetComponent<PlayerShooter>();
+    }
+
+    protected override void OnEnable() {
+        // LivingEntity의 OnEnable() 실행 (상태 초기화)
+        base.OnEnable();
+
+        healthSlider.gameObject.SetActive(true);
+        healthSlider.maxValue = startingHealth;
+        healthSlider.value = health;
+
+        playerMovement.enabled = true;
+        playerShooter.enabled = true;
+    }
+
+    // 체력 회복
+    public override void RestoreHealth(float newHealth) {
+        // LivingEntity의 RestoreHealth() 실행 (체력 증가)
+        base.RestoreHealth(newHealth);
+        healthSlider.value = health;
+    }
+
+    // 데미지 처리
+    public override void OnDamage(float damage, Vector3 hitPoint, Vector3 hitDirection) {
+        if (!dead)
+        {
+            playerAudioPlayer.PlayOneShot(hitClip);
+        }
+        // LivingEntity의 OnDamage() 실행(데미지 적용)
+        base.OnDamage(damage, hitPoint, hitDirection);
+        healthSlider.value = health;
+    }
+
+    // 사망 처리
+    public override void Die() {
+        // LivingEntity의 Die() 실행(사망 적용)
+        base.Die();
+
+        healthSlider.gameObject.SetActive(false);
+        playerAudioPlayer.PlayOneShot(deathClip);
+        playerAnimator.SetTrigger("Die");
+
+        playerMovement.enabled = false;
+        playerShooter.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        // 아이템과 충돌한 경우 해당 아이템을 사용하는 처리
+        if (!dead)
+        {
+            IItem item = other.GetComponent<IItem>();
+
+            if (item != null)
+            {
+                item.Use(gameObject);
+                playerAudioPlayer.PlayOneShot(itemPickupClip);
+            }
+        }
+    }
+}
---
+++ byahn/week07/Zombie/Assets/Scripts/Zombie.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI; // AI, 내비게이션 시스템 관련 코드 가져오기
+
+// 좀비 AI 구현
+public class Zombie : LivingEntity
+{
+    public LayerMask whatIsTarget; // 추적 대상 레이어
+
+    private LivingEntity targetEntity; // 추적 대상
+    private NavMeshAgent navMeshAgent; // 경로 계산 AI 에이전트
+
+    public ParticleSystem hitEffect; // 피격 시 재생할 파티클 효과
+    public AudioClip deathSound; // 사망 시 재생할 소리
+    public AudioClip hitSound; // 피격 시 재생할 소리
+
+    private Animator zombieAnimator; // 애니메이터 컴포넌트
+    private AudioSource zombieAudioPlayer; // 오디오 소스 컴포넌트
+    private Renderer zombieRenderer; // 렌더러 컴포넌트
+
+    public float damage = 20f; // 공격력
+    public float timeBetAttack = 0.5f; // 공격 간격
+    private float lastAttackTime; // 마지막 공격 시점
+
+    // 추적할 대상이 존재하는지 알려주는 프로퍼티
+    private bool hasTarget {
+        get
+        {
+            // 추적할 대상이 존재하고, 대상이 사망하지 않았다면 true
+            if (targetEntity != null && !targetEntity.dead)
+            {
+                return true;
+            }
+
+            // 그렇지 않다면 false
+            return false;
+        }
+    }
+
+    private void Awake() {
+        // 초기화
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        zombieAnimator = GetComponent<Animator>();
+        zombieAudioPlayer = GetComponent<AudioSource>();
+
+        zombieRenderer = GetComponentInChildren<Renderer>();
+    }
+
+    // 좀비 AI의 초기 스펙을 결정하는 셋업 메서드
+    public void Setup(ZombieData zombieData) {
+        startingHealth = zombieData.health;
+        health = zombieData.health;
+        damage = zombieData.damage;
+
+        navMeshAgent.speed = zombieData.speed;
+        zombieRenderer.material.color = zombieData.skinColor;
+    }
+
+    private void Start() {
+        // 게임 오브젝트 활성화와 동시에 AI의 추적 루틴 시작
+        StartCoroutine(UpdatePath());
+    }
+
+    private void Update() {
+        // 추적 대상의 존재 여부에 따라 다른 애니메이션 재생
+        zombieAnimator.SetBool("HasTarget", hasTarget);
+    }
+
+    // 주기적으로 추적할 대상의 위치를 찾아 경로 갱신
+    private IEnumerator UpdatePath() {
+        // 살아 있는 동안 무한 루프
+        while (!dead)
+        {
+            if (hasTarget)
+            {
+                navMeshAgent.isStopped = false;
+                navMeshAgent.SetDestination(targetEntity.transform.position);
+            }
+            else
+            {
+                navMeshAgent.isStopped = true;
+
+                Collider[] colliders = Physics.OverlapSphere(transform.position, 20f, whatIsTarget);
+                for (int i = 0; i < colliders.Length; i++)
+                {
+                    LivingEntity livingEntity = colliders[i].GetComponent<LivingEntity>();
+
+                    if (livingEntity != null && !livingEntity.dead)
+                    {
+                        targetEntity = livingEntity;
+
+                        break;
+                    }
+                }
+            }
+            // 0.25초 주기로 처리 반복
+            yield return new WaitForSeconds(0.25f);
+        }
+    }
+
+    // 데미지를 입었을 때 실행할 처리
+    public override void OnDamage(float damage, Vector3 hitPoint, Vector3 hitNormal) {
+        if (!dead)
+        {
+            hitEffect.transform.position = hitPoint;
+            hitEffect.transform.rotation = Quaternion.LookRotation(hitNormal);
+            hitEffect.Play();
+
+            zombieAudioPlayer.PlayOneShot(hitSound);
+        }
+        // LivingEntity의 OnDamage()를 실행하여 데미지 적용
+        base.OnDamage(damage, hitPoint, hitNormal);
+    }
+
+    // 사망 처리
+    public override void Die() {
+        // LivingEntity의 Die()를 실행하여 기본 사망 처리 실행
+        base.Die();
+
+        Collider[] zombieColliders = GetComponents<Collider>();
+        for (int i = 0; i < zombieColliders.Length; i++)
+        {
+            zombieColliders[i].enabled = false;
+        }
+
+        navMeshAgent.isStopped = true;
+        navMeshAgent.enabled = false;
+
+        zombieAnimator.SetTrigger("Die");
+        zombieAudioPlayer.PlayOneShot(deathSound);
+    }
+
+    private void OnTriggerStay(Collider other) {
+        // 트리거 충돌한 상대방 게임 오브젝트가 추적 대상이라면 공격 실행
+        if (!dead && Time.time >= lastAttackTime + timeBetAttack)
+        {
+            LivingEntity attackTarget = other.GetComponent<LivingEntity>();
+
+            if (attackTarget != null && attackTarget == targetEntity)
+            {
+                lastAttackTime = Time.time;
+                Vector3 hitPoint = other.ClosestPoint(transform.position);
+                Vector3 hitNormal = transform.position - other.transform.position;
+
+                attackTarget.OnDamage(damage, hitPoint, hitNormal);
+            }
+        }
+    }
+}
